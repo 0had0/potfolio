b564553a90780bc4a17a6482349db38d71e64aa9
0had0 <hadihoussainy75@gmail.com>
2020-01-10 13:29:16 +0200

    Updates

---
+++ precache-manifest.4f2d8aeadc001eb9e18d35ca2ff565f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02c7b8ea6604c75a4a2f27f1f60ad70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "40f0d9e59067add071af",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "14b47c67016c1bb31a83",
+    "url": "/portfolio/static/css/main.7ff6b36f.chunk.css"
+  },
+  {
+    "revision": "40f0d9e59067add071af",
+    "url": "/portfolio/static/js/2.d94c4b4b.chunk.js"
+  },
+  {
+    "revision": "12460f9fb46663ff18554253ea9bc158",
+    "url": "/portfolio/static/js/2.d94c4b4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14b47c67016c1bb31a83",
+    "url": "/portfolio/static/js/main.b3817e36.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  }
+]);
